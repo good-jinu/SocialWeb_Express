aa782aea91dc783295c2aaa65c6dc38b685a46fc
Jinu <gooday6983@gmail.com>
2021-07-04 17:50:52 +0900

    add sql

---
+++ database.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE `SocialWeb`;
+
+use `SocialWeb`;
+
+drop table if exists `users`;
+
+create table `users` (
+  `user` varchar(25) not null,
+  `password` varchar(30) not null,
+  primary key(`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+drop table if exists `contents`;
+
+create table `contents` (
+  `contentCode` int(11) not null,
+  `title` varchar(50) not null,
+  `main_content` text,
+  `uploadTime` timestamp not null,
+  `user` varchar(25) not null,
+  primary key(`contentCode`, `user`),
+  foreign key (`user`) references `users`(`user`) on update CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
