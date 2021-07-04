2b0630b94ed7e0e61b68f3f32b89ac756d6d8e4d
JN <gooday6983@gmail.com>
2021-07-04 14:57:51 +0900

    Updates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const users = require('./users.js');
+
+router.use('/users', users);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MyWeb' });
 });
 
 module.exports = router;
