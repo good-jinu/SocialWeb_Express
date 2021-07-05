0e83bd0e5c24df38c73cb092d0eb0a2353d3d116
Jinu <gooday6983@gmail.com>
2021-07-05 18:15:28 +0900

    Add login page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const users = require('./users.js');
+const login = require('./login.js');
 
-router.use('/users', users);
+router.use('/login', login);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MyWeb' });
+  res.render('index', { title: 'MyWeb',
+    sec_con: 'main'}
+  );
 });
 
 module.exports = router;
